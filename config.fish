48e7e97a4f67c55a415e0e23f77869b53e073661
Piotr Jasiun <piotr.jasiun@codility.com>
2019-12-09 09:54:15 +0100

    Changes for the codility workspace configuration.

--- config.fish
+++ config.fish
@@ -7,3 +7,6 @@ set -x LC_CTYPE en_US.UTF-8
 
 set -gx PATH $PATH ~/Library/Android/sdk/platform-tools/
 set -gx PATH $PATH $HOME/workspace/setup/bin/
+
+set -x VIRTUALFISH_DEFAULT_PYTHON (which python3)
+eval (python3 -m virtualfish auto_activation)
